505ad2273dec7d306027b8e54e797a2ace9f0933
suyeon <sroovy@naver.com>
2021-09-09 21:42:58 +0900

    [Update] perf ➡️ aPerformance 명확한 변수명으로 변경

--- REFACTORING/CHAPTER01/statement.js
+++ REFACTORING/CHAPTER01/statement.js
@@ -61,22 +61,22 @@ function statement(invoice, plays) {
 // 총액: $1,730.00
 // 적립 포인트: 47점
 
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) { // 명확한 이름으로 변경
   let result = 0;
 
   switch(play.type) {
     case "tragedy": // 비극
       result = 40000;
-      if(perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if(aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy": // 희극
       result = 30000;
-      if(perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if(aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`)
