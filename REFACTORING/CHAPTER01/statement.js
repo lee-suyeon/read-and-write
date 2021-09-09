a2e524490311bf2c165619c0916bf1a9a7d747db
suyeon <sroovy@naver.com>
2021-09-09 22:02:16 +0900

    [Update] amountFor()함수에 playFor() 호출로 변경

--- REFACTORING/CHAPTER01/statement.js
+++ REFACTORING/CHAPTER01/statement.js
@@ -57,7 +57,7 @@ function statement(invoice, plays) {
 function amountFor(aPerformance, play) { // 명확한 이름으로 변경
   let result = 0;
 
-  switch(play.type) {
+  switch(playFor(aPerformance).type) { // play를 playFor() 호출로 변경
     case "tragedy": // 비극
       result = 40000;
       if(aPerformance.audience > 30) {
@@ -72,7 +72,7 @@ function amountFor(aPerformance, play) { // 명확한 이름으로 변경
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`알 수 없는 장르: ${play.type}`)
+      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`); // play를 playFor() 호출로 변경
   }
   return result; // 함수의 반환값 
 }
