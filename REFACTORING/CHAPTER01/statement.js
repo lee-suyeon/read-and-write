ee81c3804bae0bbfbb88a113eb5397c4267a5090
suyeon <sroovy@naver.com>
2021-09-30 23:07:53 +0900

    [Update] 본문 전체를 별도 함수 renderPlainText로 추출

--- REFACTORING/CHAPTER01/statement.js
+++ REFACTORING/CHAPTER01/statement.js
@@ -25,75 +25,77 @@ let invoice =
 
 
 // 공연료 청구서를 출력하는 코드
-function statement(invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer})`
+function statement(invoice, plays) { // 본문 전체를 별도 함수로 추출
+  return renderPlainText(invoice, plays)
+}
 
+function renderPlainText(invoice, plays) { // 본문 전체를 별도 함수로 추출
+  let result = `청구 내역 (고객명: ${invoice.customer})`
   for(let perf of invoice.performances) {
-    // 청구 내역을 출력한다. 
-    result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`; // thisAmount 변수를 인라인
+    result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`;
   }
-  result += `총액: ${usd(totalAmount())}\n`; // 변수 인라인 후 함수 이름 바꾸기
+  result += `총액: ${usd(totalAmount())}\n`; 
   result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
   return result;
-}
 
-function amountFor(aPerformance) { // play 매개 변수 제거 
-  let result = 0;
+  function amountFor(aPerformance) { 
+    let result = 0;
 
-  switch(playFor(aPerformance).type) { 
-    case "tragedy": // 비극
-      result = 40000;
-      if(aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if(aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    switch(playFor(aPerformance).type) { 
+      case "tragedy": // 비극
+        result = 40000;
+        if(aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if(aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result; // 함수의 반환값 
   }
-  return result; // 함수의 반환값 
-}
 
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-// volumeCredits의 복제본을 초기화한 뒤 계산 결과를 반환
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  if("commedy" === playFor(aPerformance).type)
-  result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", 
-                      { style: "currency", currency: "USD",
-                        minimumFractionDigits: 2 }).format(aNumber/100);
-}
-
-function totalVolumeCredits() {
-  let result = 0; // 변수 선언(초기화)을 반복문 앞으로 이동
-  for(let perf of invoice.performances) { // 값 누적 로직을 별도 for 문으로 분리
-    result += volumeCreditsFor(perf);
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
-  return result;
-}
 
-function totalAmount() { // 함수 이름 변경
-  let result = 0;
-  for(let perf of invoice.performances) {
-    result += amountFor(perf);
+  // volumeCredits의 복제본을 초기화한 뒤 계산 결과를 반환
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    if("commedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
-  return result;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", 
+                        { style: "currency", currency: "USD",
+                          minimumFractionDigits: 2 }).format(aNumber/100);
+  }
+
+  function totalVolumeCredits() {
+    let result = 0; // 변수 선언(초기화)을 반복문 앞으로 이동
+    for(let perf of invoice.performances) { // 값 누적 로직을 별도 for 문으로 분리
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() { // 함수 이름 변경
+    let result = 0;
+    for(let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 }
 
 statement(invoice, plays)
